68884eb3d4e138c732685ab82ebf0efd28ef7efe
Isabela Pinto <isabelasouzapinto@gmail.com>
2022-02-03 21:22:36 -0300

    Add post exemples

---
+++ post.py
@@ -0,0 +1,28 @@
+from typing import Optional
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    name: str
+    description: Optional[str] = None
+    price: float
+    tax: Optional[float] = None
+
+
+app = FastAPI()
+
+
+@app.post("/items/")
+async def create_item(item: Item):
+    return item
+
+
+@app.post("/items_1/")
+async def create_item_1(item: Item):
+    item_dict = item.dict()
+    if item.tax:
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
